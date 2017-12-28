124cfd7d83f5dadc5bed789ce80157227da21068
vincentamb <33020904+vincentamb@users.noreply.github.com>
2017-12-28 18:50:21 +0530

    Initial commit

---
+++ MVCDemo/Controllers/EmployeeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BusinessLayer;
+
+namespace MVCDemo.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            EmployeeBusinessLayer employeeBusinessLayer = new EmployeeBusinessLayer();
+            List<Employee> employees = employeeBusinessLayer.Employees.ToList();
+            return View(employees);
+        }
+        [HttpGet]
+        [ActionName("Create")]
+        public ActionResult Create_Get()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Create")]
+        public ActionResult Create_Post()
+        {
+            if (ModelState.IsValid)
+            {
+                Employee employee = new Employee();
+                UpdateModel(employee);
+                EmployeeBusinessLayer employeeBusinessLayer = new EmployeeBusinessLayer();
+                employeeBusinessLayer.AddEmployee(employee);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
---
+++ MVCDemo/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+            ViewData["Countries"] =  new List<string>()
+            {
+                "India",
+                "USA",
+                "UK",
+                "Canada"
+            };
+            return View();
+        }
+    }
+}
